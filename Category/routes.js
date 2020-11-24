9ed82446521d6d39a3c9e1faeaded5ec15549e1a
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-11-24 17:46:56 -0300

    Linkando rota de edicao e criando tela de edicao

--- Category/routes.js
+++ Category/routes.js
@@ -21,6 +21,63 @@ route.get("/", (req, res) => {
     })
 })
 
+route.get("/deletar/:id", (req, res) => {
+
+    let { id } = req.params
+
+    if (id != undefined)
+    {
+        Category.destroy( { where: { id } } )
+        .then(() => res.redirect("/app/categorias"))
+        .catch((error) => {
+            console.log("categoryDestroyError: " + error)
+            res.redirect("/app/categorias")
+        })
+    }
+    else
+    {
+        res.redirect("/app/categorias")
+    }
+})
+
+route.get("/editar/:id", (req, res) => {
+
+    const { id } = req.params
+
+    if (id != undefined)
+    {
+        if (!isNaN(id))
+        {
+            Category.findByPk(id)
+            .then((category) => {
+
+                res.render("app/categories/edit.ejs", { category })
+                return true
+            })
+            .catch((error) => {
+
+                console.log("findByPrimaryKeyCategoryError: " + error)
+                res.redirect("/app/categorias")
+                return false
+            })
+        }
+        else
+        {
+            res.redirect("/app/categorias")
+            return false
+        }
+    }
+    else
+    {
+        res.redirect("/app/categorias")
+        return false
+    }
+})
+
+route.get("/editar/:id", (req, res) => {
+    
+})
+
 route.get("/nova", (req, res) => {
 
     res.render("app/categories/new.ejs")
